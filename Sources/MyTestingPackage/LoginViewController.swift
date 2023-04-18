87c1d3140c30c29bd4dbbc6c8cb1981d08df66ba
BriamCM <briam_cm14@outlook.com>
2023-04-18 10:51:41 -0600

    Adding the required Init

--- Sources/MyTestingPackage/LoginViewController.swift
+++ Sources/MyTestingPackage/LoginViewController.swift
@@ -16,7 +16,13 @@ public class LoginViewController: UIViewController {
         }
     }
     
-    public init() {}
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
