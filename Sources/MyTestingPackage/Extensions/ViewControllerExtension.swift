efee9d77477aef412362875516de7338057bcbf9
BriamCM <briam_cm14@outlook.com>
2023-04-24 16:17:21 -0600

    Correct Bundle path

--- Sources/MyTestingPackage/Extensions/ViewControllerExtension.swift
+++ Sources/MyTestingPackage/Extensions/ViewControllerExtension.swift
@@ -13,7 +13,7 @@ extension UIViewController {
     class func instanceFrom(storyboard storyB: String) -> UIViewController {
         if let path = Bundle.main.resourcePath {
             let className = String(describing: self)
-            let packagePath = path.appending("/MyTestingPackage/MyTestingPackage/Sources/MyTestingPackage")
+            let packagePath = path.appending("/MyTestingPackage/MyTestingPackage/Sources/MyTestingPackage/LoginController/StartLoginViewController.swift")
             let bundlePackage = Bundle(path: packagePath)
             let storyBoard = UIStoryboard(name: storyB, bundle: bundlePackage)
             
