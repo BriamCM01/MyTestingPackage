63603f6635904ab11d402543581389f65b7c55b4
BriamCM <briam_cm14@outlook.com>
2023-04-24 17:03:34 -0600

    Add className

--- Sources/MyTestingPackage/Extensions/ViewControllerExtension.swift
+++ Sources/MyTestingPackage/Extensions/ViewControllerExtension.swift
@@ -13,8 +13,10 @@ extension UIViewController {
     class func instanceFrom(storyboard storyB: String) -> UIViewController {
         if let path = Bundle.main.resourcePath {
             
-            print("Este es el path: \(path)")
+            
             let className = String(describing: self)
+            
+            print("Este es el path: \(path)/\(className)" )
             let packagePath = path.appending("/MyTestingPackage/MyTestingPackage/Sources/MyTestingPackage/LoginController/StartLoginViewController.swift")
             let bundlePackage = Bundle(path: packagePath)
             let storyBoard = UIStoryboard(name: storyB, bundle: bundlePackage)
