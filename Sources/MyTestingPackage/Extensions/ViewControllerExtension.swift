8018489e0e8fe35f336957399d60835073265768
BriamCM <briam_cm14@outlook.com>
2023-04-24 16:57:23 -0600

    Print path

--- Sources/MyTestingPackage/Extensions/ViewControllerExtension.swift
+++ Sources/MyTestingPackage/Extensions/ViewControllerExtension.swift
@@ -12,12 +12,17 @@ extension UIViewController {
     
     class func instanceFrom(storyboard storyB: String) -> UIViewController {
         if let path = Bundle.main.resourcePath {
+            
+            print("Este es el path: \(path)")
             let className = String(describing: self)
             let packagePath = path.appending("/MyTestingPackage/MyTestingPackage/Sources/MyTestingPackage/LoginController/StartLoginViewController.swift")
             let bundlePackage = Bundle(path: packagePath)
             let storyBoard = UIStoryboard(name: storyB, bundle: bundlePackage)
             
+            
+            
             return storyBoard.instantiateViewController(withIdentifier: className)
+            
         }
         
         return UIViewController()
